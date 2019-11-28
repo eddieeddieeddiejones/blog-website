d9dc6787bc562a2feb6d87f19e7390b6d80e6aa1
EddieJones <eddieeddiejones@gmail.com>
2019-11-28 11:48:49 +0800

    feature: create a comment(undone)

--- awesome-python3-webapp/www/handlers.py
+++ awesome-python3-webapp/www/handlers.py
@@ -234,3 +234,16 @@ def api_delete_blog(request, *, id):
     yield from blog.remove()
     return dict(id=id)
 
+@post('/api/blogs/{id}/comments')
+def api_create_comment(id, request, *, content):
+    user = request.__user__
+    if user is None:
+        raise APIPermissionError('Please signin first.')
+    if not content or not content.strip():
+        raise APIValueError('content')
+    blog = yield from Blog.find(id)
+    if blog is None:
+        raise APIResourceNotFoundError('Blog')
+    comment = Comment(blog_id=blog.id, user_id=user.id, user_name=user.name, user_image=user.image, content=content.strip())
+    yield from comment.save()
+    return comment
